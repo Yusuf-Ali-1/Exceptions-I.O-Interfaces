c05426b2f6cf207dbf0343372899bec816d224a9
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-30 14:34:44 -0400

    first commit

--- src/main/java/com/tts/exceptions/SayProperGreetingException.java
+++ src/main/java/com/tts/exceptions/SayProperGreetingException.java
@@ -5,7 +5,7 @@ public class SayProperGreetingException extends Exception {
 
     public SayProperGreetingException(String greeting) {
         this.greeting = greeting;
-        System.out.println(greeting + " is not an appropiate greeting.");
+        System.out.println(greeting + " is not an appropriate greeting.");
     }
 
     public String sayHello() {
